83c385631ad13a1801a4a00f21e0e9afcd766c57
petrostrak <pit.trak@gmail.com>
2022-06-20 18:25:21 +0300

    Setting up a simple web application

---
+++ cmd/web/app.go
@@ -0,0 +1,17 @@
+package main
+
+func StartApp() {
+	// connect to DB
+
+	// create sessions
+
+	// create channels
+
+	// create waitGroup
+
+	// setup the app config
+
+	// setup mail
+
+	// listen for web connections
+}
