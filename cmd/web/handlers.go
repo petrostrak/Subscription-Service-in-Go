a7c2d88805e4b6292fc96cbc517d963cefe09b64
petrostrak <pit.trak@gmail.com>
2022-06-21 09:58:25 +0300

    Setting up templates and building a render function

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,4 +2,6 @@ package main
 
 import "net/http"
 
-func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {}
+func (app *Config) HomePage(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "home.page.gohtml", nil)
+}
