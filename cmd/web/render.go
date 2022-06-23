455b2dad68fbaca7c4779965fae6d89c8b33e295
petrostrak <pit.trak@gmail.com>
2022-06-23 14:46:38 +0300

    Giving user data to our templates

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -5,6 +5,8 @@ import (
 	"html/template"
 	"net/http"
 	"time"
+
+	"github.com/petrostrak/Subscription-Service-in-Go/data"
 )
 
 var (
@@ -21,7 +23,7 @@ type TemplateData struct {
 	Error         string
 	Authenticated bool
 	Now           time.Time
-	// User          *data.User
+	User          *data.User
 }
 
 func (app *Config) render(w http.ResponseWriter, r *http.Request, t string, td *TemplateData) {
@@ -64,7 +66,12 @@ func (app *Config) AddDefaultData(td *TemplateData, r *http.Request) *TemplateDa
 	td.Error = app.Session.PopString(r.Context(), "error")
 	if app.IsAuthenticated(r) {
 		td.Authenticated = true
-		// TODO get more user info
+		user, ok := app.Session.Get(r.Context(), "user").(data.User)
+		if !ok {
+			app.ErrorLog.Println("can't get user from session")
+		} else {
+			td.User = &user
+		}
 	}
 	td.Now = time.Now()
 
