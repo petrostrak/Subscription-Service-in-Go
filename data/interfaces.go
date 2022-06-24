f49c35bd9eaf884cd65b1c25b029d3522bb653cb
petrostrak <pit.trak@gmail.com>
2022-06-24 19:11:22 +0300

    Modifying the data package to make it testable

---
+++ data/interfaces.go
@@ -0,0 +1,20 @@
+package data
+
+type UserInterface interface {
+	GetAll() ([]*User, error)
+	GetByEmail(string) (*User, error)
+	GetOne(int) (*User, error)
+	Update() error
+	Delete() error
+	DeleteByID(int) error
+	Insert(User) (int, error)
+	ResetPassword(string) error
+	PasswordMatches(string) (bool, error)
+}
+
+type PlanInterface interface {
+	GetAll() ([]*Plan, error)
+	GetOne(int) (*Plan, error)
+	SubscribeUserToPlan(User, Plan) error
+	AmountForDisplay() string
+}
